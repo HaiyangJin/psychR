3c5405e7ea37322999a5a97b66f35e5db60a6400
Haiyang Jin <hjin317@aucklanduni.ac.nz>
2024-05-23 19:11:34 +0800

    update to use custom contrast coding

--- R/set_contr.R
+++ R/set_contr.R
@@ -1,21 +1,31 @@
 
 #' Apply successive difference contrast coding
 #' @description
-#' Apply successive difference contrast coding (\code{MASS::contr.sdif()}) to
+#' Apply successive difference contrast coding ([MASS::contr.sdif()]) to
 #' variables in data and create variables to be used in steps of obtaining the
 #' optimal mixed-effects models.
 #'
 #' @param .data A data frame, data frame extension (e.g. a tibble), or a lazy
 #' data frame (e.g. from \code{dbplyr} or \code{dtplyr})
 #' @param varnames the columns to be applied contrasting code (without quotes)
+#' @param contraFUN the contrast coding function (name) to be used.
+#' Default to \code{MASS::contr.sdif}. Currently popular contrast
+#' coding functions are: [MASS::contr.sdif](default), [contr.sum],
+#' [contr.treatment], [contr.SAS], etc.
 #'
 #' @return A data frame.
 #' @export
 #'
 #' @examples
-#' set_sdif(jin2022noncon, Congruency) |> str()
-#' set_sdif(jin2022noncon, c(Congruency, Alignment)) |> str()
-set_sdif <- function(.data, varnames){
+#' set_contr(jin2022noncon, Congruency) |> str()
+#'
+#' set_contr(jin2022noncon, c(Congruency, Alignment)) |> str()
+#'
+#' set_contr(jin2022noncon, c(Congruency, Alignment), contr.sum) |> str()
+#'
+set_contr <- function(.data,
+                     varnames,
+                     contraFUN=MASS::contr.sdif){
 
   varnames <- deparse(substitute(varnames)) |>
     stringr::str_remove("c\\(") |>
@@ -31,7 +41,7 @@ set_sdif <- function(.data, varnames){
     message(sprintf("There are %d levels in %s.", N, vn))
 
     # apply contrast coding
-    contrasts(.data[[vn]]) <- MASS::contr.sdif(N)
+    contrasts(.data[[vn]]) <- contraFUN(N)
 
   }
 
